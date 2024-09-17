e7810e6b0917550ab377af28a5b2250aedddb892
ZoDSeR SyLVaN <30656356+ZoDSeR97@users.noreply.github.com>
2024-09-17 11:41:31 -0400

    rehearse C#

---
+++ cs/TwoSum/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+
+Each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+namespace TwoSum;
+
+class Program
+{
+    public static int[] Solution(int[] nums, int target)
+    {
+        /**
+        Use hashmap to avoid reiterate
+        Time: O(n)
+        Space: O(n) 
+        */
+        var set = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (set.ContainsKey(target - nums[i]))
+                return [i, set[target - nums[i]]];
+            set[nums[i]] = i;
+        }
+        return [0, 0]; //should never reach here if there is a solution
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(String.Join(", ", Solution([2, 7, 11, 15], 9)));
+    }
+}
---
+++ cs/ValidPalindrome/Program.cs
@@ -0,0 +1,50 @@
+﻿/**
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters 
+and removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+*/
+
+namespace ValidPalindrome;
+
+class Program
+{
+    public static bool Solution(string s)
+    {
+        /**
+        Time: O(n)
+        Space: O(1) 
+        */
+        for (int i = 0, j = s.Length-1; j>i;)
+        {
+            if (!char.IsLetterOrDigit(s[i]))
+            {
+                i++;
+                continue;
+            }
+            if (!char.IsLetterOrDigit(s[j]))
+            {
+                j--;
+                continue;
+            }
+            if (char.ToLower(s[i++]) != char.ToLower(s[j--])) 
+                return false;
+        }
+        return true;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution("A man, a plan, a canal: Panama"));
+    }
+}
