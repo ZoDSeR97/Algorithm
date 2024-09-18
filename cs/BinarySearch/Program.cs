8cefbddc2d0066bbf54fc7568e06d04e1694fd44
ZoDSeR SyLVaN <30656356+ZoDSeR97@users.noreply.github.com>
2024-09-18 09:20:33 -0400

    rehearse C#

---
+++ cs/BinarySearch/Program.cs
@@ -0,0 +1,43 @@
+﻿/**
+Given an array of integers nums which is sorted in ascending order, and an integer target, 
+write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+*/
+namespace BinarySearch;
+
+class Program
+{
+    public static int Solution(int[] nums, int target)
+    {
+        /**
+        Time: O(logn)
+        Space: O(1)
+        */
+        for (int i = 0, j = nums.Length-1; i < j;)
+        {
+            int mid = (i+j)/2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] > target)
+                j=mid-1;
+            else
+                i=mid+1;
+        }
+        return -1;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution([-1, 0, 3, 5, 9, 12], 9));
+    }
+}
---
+++ cs/InvertBinaryTree/Program.cs
@@ -0,0 +1,47 @@
+﻿/**
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+*/
+
+namespace InvertBinaryTree;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Program
+{
+    static TreeNode Solution(TreeNode root)
+    {
+        if (root == null)
+            return null;
+        TreeNode temp = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(temp);
+        return root;
+    }
+    static void Main(string[] args)
+    {
+        TreeNode treeNode= new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(3);
+        Solution(treeNode);
+        Console.WriteLine("Hello, World!");
+    }
+}
