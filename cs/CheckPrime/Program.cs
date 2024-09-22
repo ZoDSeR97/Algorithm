c936269d72d2d21941f571492f92d3eb2a5cf0ee
ZoDSeR SyLVaN <30656356+ZoDSeR97@users.noreply.github.com>
2024-09-22 08:58:58 -0400

    rehearse C#

---
+++ cs/CheckPrime/Program.cs
@@ -0,0 +1,40 @@
+﻿/**
+You are given an integer number n, check if n is prime.
+*/
+
+namespace CheckPrime;
+
+class Program
+{
+    static bool Solution(int n)
+    {
+        /**
+        Trial Division
+        Time: O(sqrt(n))
+        Space: O(1)
+        */
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        /**
+        Unoptimize version
+        Time: O(n)
+        Space: O(1) *
+        for (int i = 2; i < n; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        */
+        return true;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution(30));
+    }
+}
---
+++ cs/PrimeSieves/Program.cs
@@ -0,0 +1,47 @@
+﻿/**
+You are given an integer number n, finding all primes up to n.
+*/
+
+namespace PrimeSieves;
+
+class Program
+{
+    static List<int> Solution(int n)
+    {
+        /**
+        Sieve of Eratosthenes algorithm
+        Time: O(n log log n)
+        Space: O(n)
+        */
+        bool[] isPrime = new bool[n+1];
+        for (int i = 2; i < n+1; i++)
+        {
+            isPrime[i] = true;
+        }
+
+        for (int p = 2; p * p < n+1; p++)
+        {
+            // If isPrime[p] is not changed, then it is a prime
+            if (isPrime[p])
+                // Updating all multiples of p to not prime
+                for (int i = p * p; i < n + 1; i += p)
+                {
+                    isPrime[i] = false;
+                }
+        }
+
+        // Collecting all prime numbers
+        List<int> primes = new List<int>();
+        for (int i = 2; i < n + 1; i++)
+        {
+            if (isPrime[i])
+                primes.Add(i);
+        }
+
+        return primes;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(String.Join(", ", Solution(30)));
+    }
+}
